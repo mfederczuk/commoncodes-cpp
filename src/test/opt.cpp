bb0bebf6f999b4cb069ccf02867a90a70ca8d423
Michael Federczuk <federczuk.michael@protonmail.com>
2020-05-03 01:32:17 +0200

    Add tests for opt::builder

--- src/test/opt.cpp
+++ src/test/opt.cpp
@@ -6,6 +6,7 @@
 namespace cc = commoncodes;
 using cc::opt_id;
 using cc::opt;
+using cc::unknown_opt_id;
 using std::string;
 using std::vector;
 
@@ -33,4 +34,23 @@ int main() {
 
 	assert(opt1 == opt(opt_id1, {'f'}, {"foo"}));
 	assert(opt1 != opt2);
+
+
+	assert(opt() == opt::builder().build());
+
+	assert(opt(opt_id1, {}, {}) == opt::builder().id(opt_id1).build());
+	assert(opt(opt_id('a'), {}, {}) == opt::builder().id('a').build());
+	assert(opt(opt_id("abc"), {}, {}) == opt::builder().id("abc").build());
+
+	assert(opt(unknown_opt_id, {'a'}, {}) == opt::builder().alias('a').build());
+	assert(opt(unknown_opt_id, {}, {"abc"}) == opt::builder().alias("abc").build());
+	assert(opt(unknown_opt_id, {'a'}, {"abc"}) == opt::builder().alias('a', "abc").build());
+
+	assert(opt(unknown_opt_id, {'a', 'b'}, {}) == opt::builder().short_aliases({'a', 'b'}).build());
+	assert(opt(unknown_opt_id, {}, {"abc", "xyz"}) == opt::builder().long_aliases({"abc", "xyz"}).build());
+	assert(opt(unknown_opt_id, {'a', 'b'}, {"abc", "xyz"}) == opt::builder().aliases({'a', 'b'}, {"abc", "xyz"}).build());
+
+	assert(opt(unknown_opt_id, {}, {}) == opt::builder().no_arg().build());
+	assert(opt(unknown_opt_id, {}, {}, true) == opt::builder().arg_required().build());
+	assert(opt(unknown_opt_id, {}, {}, false) == opt::builder().arg_optional().build());
 }
