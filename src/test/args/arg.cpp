baa28ffb193754a6b2ea49e351c3f51834ae7c00
Michael Federczuk <federczuk.michael@protonmail.com>
2020-05-03 18:31:15 +0200

    Add test for arg

---
+++ src/test/args/arg.cpp
@@ -0,0 +1,46 @@
+#include <cassert>
+#include <commoncodes/bits/args/arg.hpp>
+#include <optional>
+#include <string>
+
+namespace cc = commoncodes;
+using cc::arg;
+using cc::opt_arg;
+using cc::opt_id;
+using cc::opt;
+using cc::val_arg;
+using std::nullopt;
+using std::string;
+
+int main() {
+	string s1 = "foobar";
+	val_arg va1 = val_arg(s1);
+	arg a1 = arg(va1);
+
+	assert(a1.is_val());
+	assert(a1.val() == va1);
+
+	val_arg va2 = a1;
+	assert(va1 == va2);
+
+
+	opt o1 = opt(opt_id(0), {'f'}, {"foo"});
+	string s2 = "-f";
+	opt_arg oa1 = opt_arg(o1, s2, nullopt);
+	arg a2 = arg(oa1);
+
+	assert(a2.is_opt());
+	assert(a2.opt() == oa1);
+
+	opt_arg oa2 = a2;
+	assert(oa1 == oa2);
+
+
+	arg a3 = arg();
+	assert(a3.is_val());
+	assert(a3.val() == val_arg());
+
+
+	assert(arg(val_arg(s1)) == a1);
+	assert(a1 != a2);
+}
