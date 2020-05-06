578f1a4cc2abc40830706fd5cf1c11ab0c8ce3f3
Michael Federczuk <federczuk.michael@protonmail.com>
2020-05-06 12:01:47 +0200

    Fix opt_arg test

--- src/test/args/opt_arg.cpp
+++ src/test/args/opt_arg.cpp
@@ -14,17 +14,17 @@ using std::string;
 
 int main() {
 	opt opt1 = opt(opt_id(0), {'f'}, {"foo"});
-	string s1 = "--foo";
+	string s1 = "foo";
 	opt_arg oa1 = opt_arg(opt1, s1, nullopt);
 
 	assert(oa1.opt() == opt1);
-	assert(oa1.alias_arg() == s1);
+	assert(oa1.alias_arg() == "--" + s1);
 	assert(!oa1.has_arg());
 
 	opt opt2 = opt(opt_id(1), {'b'}, {"bar"}, true);
-	string s2 = "-b";
+	char c1 = 'b';
 	string arg = "arg";
-	opt_arg oa2 = opt_arg(opt2, s2, arg);
+	opt_arg oa2 = opt_arg(opt2, c1, arg);
 
 	assert(oa2.has_arg());
 	assert(oa2.arg() == arg);
@@ -42,6 +42,6 @@ int main() {
 	assert(os.str() == "-barg");
 
 	os.str(""), os.clear();
-	os << opt_arg(opt(), "--yee", "haw");
+	os << opt_arg(opt(), "yee", "haw");
 	assert(os.str() == "--yee=haw");
 }
